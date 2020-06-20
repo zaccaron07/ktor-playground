1de352d0739debe3e826c5847a881c8bbf51c20a
Gabriel <gabrielbrogni@hotmail.com>
2020-06-20 15:59:22 -0300

    Hello world

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,19 @@
 package com.example
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello from Ktor!")
+        }
+    }
 }
-
